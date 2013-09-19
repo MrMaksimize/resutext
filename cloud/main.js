a2fee39ff4f4d1f1371830424f2468f67f2afdd8
Maksim Pecherskiy <max@maksimize.com>
2013-09-19 13:13:00 -0700

    Base stable version.

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,59 @@
 
 var Twilio = require('twilio');
+var accountSid = 'AC439fd476eb3ee822125daca41811ed03';
+var authToken = 'e45c87328232b821063880b62b023a0e';
 
 Twilio.initialize(accountSid, authToken);
 
+var generateResponse = function(command) {
+  var originalCommand = command;
+  command = command.toLowerCase();
+  if (command.indexOf("resume") != -1) {
+    return "Maksim is Awesome.  Check out his resume here: http://bit.ly/MaksimRes";
+  }
+  // Fallback
+  else {
+    var response = "Ok... I'll be honest here.  I dont know what '" + originalCommand + "' Means.";
+    var insult = "Please learn to use your keyboard.  Thanks!";
+
+    response = response + " " + insult;
+    return response;
+  }
+}
+
+var sendSMS = function(fromNum, toNum, bodyText) {
+  console.log('from: ' + fromNum);
+  console.log('to : ' + toNum);
+  console.log('text: ' + bodyText);
+  Twilio.sendSMS({
+    From: fromNum,
+    To: toNum,
+    Body: bodyText,
+  }, {
+    success: function(httpResponse) {
+      console.log(httpResponse);
+      console.log('request success');
+    },
+    error: function(httpResponse) {
+       console.error(httpResponse);
+       throw "Request went awry";
+    }
+  });
+}
+
+
 Parse.Cloud.define("incomingSMS", function(request, response) {
-  console.log(request);
+  console.log('log');
+  console.log(request.params);
+  // Get Command
+  var command = request.params.Body;
+  var responseMessageText = generateResponse(command);
+  try {
+    sendSMS(request.params.To, request.params.From, responseMessageText);
+    response.success("Response Complete");
+  } catch (error) {
+    response.error(error.message);
+  }
 });
+
+
