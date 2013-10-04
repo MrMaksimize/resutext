24d3aecbb085a51d3af6e41316524b59194bca2e
glesage <gefthefrench@gmail.com>
2013-10-04 11:02:43 -0500

    Removed excess user management that PARSE ALREADY DOES

--- cloud/user_handler.js
+++ cloud/user_handler.js
@@ -6,24 +6,6 @@ var global = require('cloud/globals.js');
 var phone_handler = require('cloud/phone.js');
 var email_handler = require('cloud/email.js');
 
-
-// -- Public User Constructor -- //
-
-exports.makeAUser = function(email, firstName, lastName, phone, resume) {
-
-  if (!email || email.length < 1)          return null;
-  if (!firstName || firstName.length < 1)  return null;
-  if (!lastName || lastName.length < 1)    return null;
-  if (!phone || phone.length < 10)         return null;
-  if (!resume || resume.length < 5)        return null;
-
-  if (phone_handler.findPhoneNumbers(phone) < 1)    return null;
-  if (email_handler.findEmailAddresses(email) < 1)  return null;
-  if (checkResumeURL(resume) < 1)                   return null;
-
-  return new USER(email, firstName, lastName, phone, resume);
-}
-
 function checkResumeURL(StrObj) {
   var resumeArray = [];
   resumeArray = StrObj.match("^(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+))*$");
@@ -95,56 +77,4 @@ exports.findUserWithEmail = function(email) {
   if (email_handler.findEmailAddresses(email) < 1)  return null;
 
   return "he!";
-}
-
-
-// -- USER OBJECT -- //
-
-/*
- * User Object, includes:
- * - Email
- * - First name
- * - Last name
- * - Phone
- * - Resume URL
- */
-function USER(email, firstName, lastName, phone, resume)
-{
-  this.email = email;
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.phone = phone;
-  this.resume = resume;
-}
-
-USER.prototype.register = function userRegister() {
-  console.log("Registering " + this);
-  
-  var user = new Parse.User();
-   
-  user.signUp({
-    username: this.email,
-    password: "resutext",
-    email: this.email,
-    phone: this.phone,
-    firstName: this.firstName,
-    lastName: this.lastName,
-    resume: this.resume
-  }, {
-    success: function(user) {
-      console.log('New user created');
-    },
-    error: function(user, error) {
-      alert("Error: " + error.code + " " + error.message);
-      console.log('Failed to create new user: ' + error.description);
-    }
-  });
-}
-
-USER.prototype.get = function userGet() {
-  return { email: this.email, firstName: this.firstName, lastName: this.lastName, phone: this.phone, resume: this.resume };
-}
-
-USER.prototype.toString = function userToString() {
-  return this.firstName + " " + this.lastName + ", Mail: " + this.email + ", Phone: " + this.phone + ", Resume at: " + this.resume;
 }
