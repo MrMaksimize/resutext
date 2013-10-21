62450cf67f103c0b6c75c34d415779f17bd455f7
Maksim Pecherskiy <max@maksimize.com>
2013-10-21 17:21:30 -0500

    Adding auth popup.

---
+++ public/javascripts/oauthslave.popup.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+  if (window.name=="ConnectWithLinkedIn") {
+    window.close();
+  }
+  $('.login-link').click(function(event) {
+    event.preventDefault();
+    var path = '/auth';
+    var windowName = 'ConnectWithLinkedIn'; // should not include space for IE
+    var windowOptions = 'location=0,status=0,width=570,height=700,scrollbars=yes';
+    var oauthWindow   = window.open(path, windowName, windowOptions);
+    var oauthInterval = window.setInterval(function(){
+      if (oauthWindow.closed) {
+        window.clearInterval(oauthInterval);
+        window.location = 'https://resutext.parseapp.com';
+      }
+    }, 1000);
+  });
+});
+
